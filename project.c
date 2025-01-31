252edccf82308af72913e72350dee50ca8877f91
Daksh Jaitly <thedakshjaitly@gmail.com>
2025-01-31 20:01:20 +0530

    First commit

---
+++ project.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+//num of student var
+int stuNum = 0;
+
+//student info variables
+struct stuInfo {
+    char fname[50];
+    char lname[50];
+    int roll;
+    float cgpa;
+    int cid[10];
+
+} st[50];
+
+
+void add_statement() {
+
+    for (int j = 0; j < 5; j++) {
+        scanf("%d", &st[stuNum].cid[j]);
+    }
+    stuNum = stuNum + 1;
+}
